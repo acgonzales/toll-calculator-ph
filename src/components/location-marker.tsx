87ea88e9ef87f6a38827f8c40d2566a0022f4144
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2025-03-04 18:00:58 +0800

    feat: ui improvement

--- src/components/location-marker.tsx
+++ src/components/location-marker.tsx
@@ -1,18 +1,36 @@
 import { MapPinIcon } from '@heroicons/react/24/solid';
-import { Marker, Popup } from 'react-map-gl/mapbox';
+import { Marker, MarkerInstance } from 'react-map-gl/mapbox';
 import { Location } from '@/types/common.types';
+import { useEffect, useMemo, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 export default function LocationMarker({ location }: { location: Location }) {
+  const markerRef = useRef<MarkerInstance>(null);
+
+  useEffect(() => {
+    if (markerRef.current && !markerRef.current.getPopup()?.isOpen()) {
+      markerRef.current.togglePopup();
+    }
+  }, []);
+
+  const popup = useMemo(
+    () =>
+      new mapboxgl.Popup({
+        anchor: 'bottom',
+        closeButton: false,
+        offset: 8,
+        className: 'text-xs text-black',
+      }).setText(location.name),
+    [location],
+  );
+
   return (
-    <Marker latitude={location.coordinates.latitude} longitude={location.coordinates.longitude}>
-      <Popup
-        latitude={location.coordinates.latitude}
-        longitude={location.coordinates.longitude}
-        closeButton={false}
-        className="text-xs text-black"
-      >
-        {location.name}
-      </Popup>
+    <Marker
+      ref={markerRef}
+      latitude={location.coordinates.latitude}
+      longitude={location.coordinates.longitude}
+      popup={popup}
+    >
       <MapPinIcon className="size-10 cursor-pointer text-red-500 hover:text-red-700" />
     </Marker>
   );

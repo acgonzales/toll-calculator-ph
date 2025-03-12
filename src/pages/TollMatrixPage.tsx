727b835bef36f1dbded34acb1b1bf71283690c51
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2025-03-12 14:19:23 +0800

    feat: faq page

--- src/pages/TollMatrixPage.tsx
+++ src/pages/TollMatrixPage.tsx
@@ -5,56 +5,54 @@ export default function TollMatrixPage() {
   const entries = Object.keys(matrix);
 
   return (
-    <>
-      <div className="rounded-box border-base-content/5 bg-base-100 overflow-x-auto border">
-        <div role="alert" className="alert alert-info alert-soft">
-          <span>Only NLEX-SCTEX Toll Matrix (Class 1), other expressways will be added soon.</span>
-        </div>
-        <table className="table-xs table-zebra table-pin-rows table-pin-cols table">
-          <thead>
-            <tr>
-              <th className="text-xs">Entry / Exit</th>
+    <div className="rounded-box border-base-content/5 bg-base-100 overflow-x-auto border">
+      <div role="alert" className="alert alert-info alert-soft">
+        <span>Only NLEX-SCTEX Toll Matrix (Class 1), other expressways will be added soon.</span>
+      </div>
+      <table className="table-xs table-zebra table-pin-rows table-pin-cols table">
+        <thead>
+          <tr>
+            <th className="text-xs">Entry / Exit</th>
+            {entries.map((exit) => (
+              <th key={exit} className="text-xs">
+                {exit}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry) => (
+            <tr key={entry}>
+              <th className="text-xs">{entry}</th>
               {entries.map((exit) => (
-                <th key={exit} className="text-xs">
-                  {exit}
-                </th>
+                <td key={`${entry}-${exit}`} className="font-mono">
+                  {matrix[entry][exit] ? (
+                    <div className="flex flex-col gap-1 text-center">
+                      <div>
+                        <span className="badge badge-soft badge-primary badge-sm">
+                          {Number(matrix[entry][exit][0]).toFixed(2)}
+                        </span>
+                      </div>
+                      <div>
+                        <span className="badge badge-soft badge-secondary badge-sm">
+                          {Number(matrix[entry][exit][1]).toFixed(2)}
+                        </span>
+                      </div>
+                      <div>
+                        <span className="badge badge-soft badge-accent badge-sm">
+                          {Number(matrix[entry][exit][2]).toFixed(2)}
+                        </span>
+                      </div>
+                    </div>
+                  ) : (
+                    '-'
+                  )}
+                </td>
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {entries.map((entry) => (
-              <tr key={entry}>
-                <th className="text-xs">{entry}</th>
-                {entries.map((exit) => (
-                  <td key={`${entry}-${exit}`} className="font-mono">
-                    {matrix[entry][exit] ? (
-                      <div className="flex flex-col gap-1 text-center">
-                        <div>
-                          <span className="badge badge-soft badge-primary badge-sm">
-                            {Number(matrix[entry][exit][0]).toFixed(2)}
-                          </span>
-                        </div>
-                        <div>
-                          <span className="badge badge-soft badge-secondary badge-sm">
-                            {Number(matrix[entry][exit][1]).toFixed(2)}
-                          </span>
-                        </div>
-                        <div>
-                          <span className="badge badge-soft badge-accent badge-sm">
-                            {Number(matrix[entry][exit][2]).toFixed(2)}
-                          </span>
-                        </div>
-                      </div>
-                    ) : (
-                      '-'
-                    )}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
